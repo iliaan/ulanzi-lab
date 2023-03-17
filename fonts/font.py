0b02ecbe26715a0eec8bc5ea9b5f5e80ce2bbc9d
iliaan <iliaan@users.noreply.github.com>
2023-03-17 08:46:04 +0000

    more fonts

--- fonts/font.py
+++ fonts/font.py
@@ -1,11 +1,21 @@
+import os
 import string
-
 from PIL import ImageFont
 from PIL import Image
 
 point_size = 16
-font = ImageFont.truetype("./fonts/TinyUnicode.ttf", point_size)
+font_path = os.path.join(".", "fonts", "3x5MatrixDisplay.ttf")
+font = ImageFont.truetype(font_path, point_size)
 
-for char in string.ascii_uppercase:
-    im = Image.Image()._new(font.getmask(char))
-    im.save("./fonts/" + char + ".bmp")
+characters = [char for char in string.ascii_uppercase]
+
+for char in characters:
+    char_box = font.getbbox(char)[2:]
+    print(char, char_box)
+    with Image.new("1", (char_box)) as im:
+        im.putdata(font.getmask(char))
+        #print image size
+        print(char, font.getbbox(char))
+        print(char, font.getlength(char))
+        print(char, font.getsize(char))
+        im.save(os.path.join(".", "fonts", char + ".bmp"))
